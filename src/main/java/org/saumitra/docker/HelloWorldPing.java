f7c2a114e26000ec63ccc88a805bbd8c1563ced8
saum <saumitr2288@gmail.com>
2018-07-04 09:28:40 +0530

    Initial commit

---
+++ src/main/java/org/saumitra/docker/HelloWorldPing.java
@@ -0,0 +1,15 @@
+package org.saumitra.docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldPing {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+}
